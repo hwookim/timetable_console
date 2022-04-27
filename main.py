6c2a0a025f456aed5801c8398a6486992fc7b2ca
hwookim <gusdn9754@gmail.com>
2022-04-27 15:02:16 +0900

    feat: 빈 함수로 대체

--- main.py
+++ main.py
@@ -7,15 +7,15 @@ def main():
         printMenu()
         value = input_number('> ', '1 ~ 6 사이의 수를 입력해주세요.')
         if value == 1:
-            print()
+            manage_subjects()
         elif value == 2:
-            print()
+            manage_lectures()
         elif value == 3:
-            print()
+            manage_rooms()
         elif value == 4:
-            print()
+            manage_timetable()
         elif value == 5:
-            print()
+            manage_data()
         elif value == 6:
             print()
             print('프로그램을 종료합니다.')
@@ -33,6 +33,26 @@ def printMenu():
     print('5. 데이터 관리')
     print('6. 종료')
     print('**********************************************************************')
+
+
+def manage_subjects():
+    print()
+
+
+def manage_lectures():
+    print()
+
+
+def manage_rooms():
+    print()
+
+
+def manage_timetable():
+    print()
+
+
+def manage_data():
+    print()
 
 
 main()
